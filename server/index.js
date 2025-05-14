b784d361231c4ec3059204526eaca9e40f5ed650
RabbulMia22 <159376424+RabbulMia22@users.noreply.github.com>
2025-05-14 09:53:26 +0600

    something change

--- server/index.js
+++ server/index.js
@@ -9,19 +9,8 @@ app.get('/', (req, res) => {
   res.send('Hello World! Coffe Server is running');
 
 });
-app.post('/coffe', (req, res) => {
-  const { name, type, size } = req.body;
-  console.log('Received data:', req.body);
-  res.json({ message: `Coffe order received! ${name} ${type} ${size}` });
-})
-app.get('/coffe', (req, res) => {
-  const coffe = [
-    { name: 'Espresso', type: 'hot', size: 'small' },
-    { name: 'Latte', type: 'hot', size: 'medium' },
-    { name: 'Iced Coffee', type: 'cold', size: 'large' }
-  ];
-  res.json(coffe);
-});
+
+
 app.listen(port, () => {
   console.log(`Coffe Server is running at http://localhost:${port}`);
 });
